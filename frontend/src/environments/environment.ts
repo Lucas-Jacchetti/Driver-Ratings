d99c40c9e146db2caced43dd15f6eba1678a9a8d
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2026-09-20 00:20:15 -0300

    aws origin allowed to cors, enviroment to cloudfront backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5030/api',
+  apiUrl: 'http://d2ibvl9xnx2c0b.cloudfront.net/api',
   googleClientId: '827888631012-nvuuvds5fd2j2s20t8se1vo16rmqnp0n.apps.googleusercontent.com',
 };
